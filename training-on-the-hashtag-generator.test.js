8fb7eb33b7bbcbfb8d0222acaad68d198ff51c18
helciodev <helcioandre@live.com>
2021-11-11 18:59:47 +0100

    Solution and more tests

--- training-on-the-hashtag-generator.test.js
+++ training-on-the-hashtag-generator.test.js
@@ -1,9 +1,29 @@
 const hashTagGenerator = require("./training-on-the-hashtag-generator");
 
 test("returns #HelloThereThanksForTryingMyKata", () => {
-  expect(
-    hashTagGenerator('"Hello there thanks for trying my Kata"').toBe(
-      "#HelloThereThanksForTryingMyKata"
-    )
+  expect(hashTagGenerator("Hello there thanks for trying my Kata")).toBe(
+    "#HelloThereThanksForTryingMyKata"
   );
+});
+
+test("returns #HelloWorld", () => {
+  expect(hashTagGenerator("Hello     World")).toBe("#HelloWorld");
+});
+
+test("return false", () => {
+  expect(hashTagGenerator("")).toBe(false);
+});
+
+test("return false if character count bigger than 140", () => {
+  expect(
+    hashTagGenerator(
+      "Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"
+    )
+  ).toBe(false);
+});
+test("return #CodewarsIsNice", () => {
+  expect(hashTagGenerator("Codewars is nice")).toBe("#CodewarsIsNice");
+});
+test("returns false is str equals(#)", () => {
+  expect(hashTagGenerator("#")).toBe(false);
 });
